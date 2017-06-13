10ecde9e9bf2ea841704981c87bb02cc0bea09ec
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-13 13:54:26 +0800

    Updated server/client test.

--- pyp-test.js
+++ pyp-test.js
@@ -2,13 +2,20 @@
 const assert = require( "assert" );
 const pyp = require( "./pyp.js" );
 
+assert.equal( pyp( [ 1, 2, 3, "hello" ], STRING ), "hello", "should return 'hello'" );
+
 assert.equal( pyp( [ 1, 2, 3 ], 2 ), 2, "should be equal" );
 
 assert.equal( pyp( [ 1, 2, 3 ], NUMBER ), 1, "should be equal" );
+
+assert.deepEqual( pyp( [ { }, 1, 2, 3, { "hello": "world" } ], OBJECT ), { }, "should return empty object" );
 
 let date = new Date( );
 assert.equal( pyp( [ true, date, { }, "hello" ], Date ), date, "should be equal" );
 
 assert.equal( pyp( [ true, date, { }, "hello" ], "Date" ), date, "should be equal" );
+
+let hello = function hello( ){ return "hello" };
+assert.deepEqual( pyp( [ 1, 2, 3, { }, hello ], FUNCTION ), hello, "should be equal" );
 
 console.log( "ok" );
--- pyp.js
+++ pyp.js
@@ -33,6 +33,9 @@
 			"module": "pyp",
 			"author": "Richeve S. Bebedor",
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/pyp.git",
 			"test": "pyp-test.js",
 			"global": true
