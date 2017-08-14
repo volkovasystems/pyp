cdc3556e71a03f216e79f48463c1cb301f156a23
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-14 13:31:44 +0800

    Updated test.

--- pyp-test.js
+++ pyp-test.js
@@ -10,10 +10,8 @@ assert.equal( pyp( [ 1, 2, 3 ], NUMBER ), 1, "should be equal 1" );
 
 assert.deepEqual( pyp( [ { }, 1, 2, 3, { "hello": "world" } ], OBJECT ), { }, "should be equal to empty object" );
 
-/* These test produces error when given condition with typeof Date
 let date = new Date( );
-assert.equal( pyp( [ true, date, { }, "hello" ], Date ), date, "should be equal to new Date( ) value" );
-*/
+assert.equal( pyp( [ true, date, { }, "hello" ], date ), date, "should be equal the give condition which is new Date( ) value" );
 
 let hello = function hello( ){ return "hello" };
 assert.deepEqual( pyp( [ 1, 2, 3, { }, hello ], FUNCTION ),
